f584bcbfcb01548e3ee2fa166b642b73a078ee5d
Oscar Gonzalez <ogonzal87@gmail.com>
2015-02-14 00:32:31 -0500

    added pages, controllers and models to the app

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+class StaticPagesController < ApplicationController
+	def home
+		@active = 'home'
+	end
+
+	def about
+		@active = 'about'
+	end
+
+	def contact
+		@active = 'contact'
+	end
+
+end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,8 @@
-class Place
+class Place < ActiveRecord::Base
+
 	def self.search
-		search_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyDN-WyapDlaLVbsJiYKWUODGJfgcN6AR34&location=33,-84&rankby=distance&keyword=coffee"
-		response = HTTParty.get(search_url)		
-		response['results']
+		search_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyDSNCeNSD64AM7KxZ3kK1kLEQcku_UYzns&location=33,-84&rankby=distance&keyword=coffee"
+		api_response = HTTParty.get(search_url)
+		api_response['results']
 	end
 end
