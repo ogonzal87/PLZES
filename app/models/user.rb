89ba1f382cf2caf18c71b40a352b70c3cc17d002
Oscar Gonzalez <ogonzal87@gmail.com>
2015-02-16 12:21:33 -0500

    made the email input all lowercase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-	has_many :places
+  before_save { email.downcase! }
+	
+  has_many :places
 
   validates :name, presence: true, length: { minimum: 3, maximum: 254 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
