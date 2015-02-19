11b9445af45ca8a4d3e757d854b08e7dfc0a8ad7
Oscar Gonzalez <ogonzal87@gmail.com>
2015-02-19 13:40:21 -0500

    reverted back to the original code after Gerry's prank

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,8 +3,7 @@ class Place < ActiveRecord::Base
 
 	def self.search(location)
 		location = URI.encode(location)
-		search_url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=chipotle+in+#{location}&key=AIzaSyDSNCeNSD64AM7KxZ3kK1kLEQcku_UYzns"
-		# search_url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=bars+in+#{location}&key=AIzaSyDSNCeNSD64AM7KxZ3kK1kLEQcku_UYzns"
+		search_url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=bars+in+#{location}&key=AIzaSyDSNCeNSD64AM7KxZ3kK1kLEQcku_UYzns"
 		api_response = HTTParty.get(search_url)
 		api_response['results']
 	end
